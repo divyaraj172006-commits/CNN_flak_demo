df179603880db2210368caafee63be55e7918399
divya <divyaraj172006@gmail.com>
2026-01-31 12:18:41 +0530

    cnn

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('index.html')
+
+@app.route("/abc")
+def abc():
+    return render_template('abc.html')
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000,use_reloader=False)
+    app.run(debug=True)
